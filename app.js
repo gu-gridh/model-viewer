f479f0d055120a28f7e1ebbd49941b019658fd26
Tristan Bridge <tbridge360@gmail.com>
2023-10-02 15:07:06 +0200

    fetch for metadata/update port

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-const PORT = 5177;
+const PORT = 8094;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
